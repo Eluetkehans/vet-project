a148ccae6b4c8ef6f7b119b2b3e4d0efd2ec04f3
Erik Luetkehans <eluetekhans@gmail.com>
2015-11-20 15:11:35 -0800

    initial test setup

---
+++ test/dog_route_test.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+var expect = chai.expect;
+var mongoose = require('mongoose');
+
+// Set test db path on local machine
+process.env.MONGO_URL = 'mongodb://localhost/pet_test';
+
+// Pull in our models
+var Pet = require(__dirname + '/models/pet');
+
+// set beggining of URL for later convenience
+var url = '/localhost:3000/api';
+
+// require in our server so it auto-runs when this test is run.
+require(__dirname + '/../server');

3c94fa9dfb5f684cf625e55d0aa0b4b61d712bb3
Erik Luetkehans <eluetekhans@gmail.com>
2015-11-25 15:51:42 -0800

    add webpack build functionality

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var webpack = require('webpack-stream');
+var Karma = require('karma').Server;
+
+// build our bundle file for webpack
+gulp.task('webpack:dev', function() {
+  return gulp.src('./app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+// build our bundle for testing
+gulp.task('webpack:test', function() {
+  return gulp.src('./test/client/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/client'));
+});
+
+// feed our webpack test build into karma
+gulp.task('karmatests', ['webpack:test'], function(done) {
+  new Karma({
+    configFile: __dirname + '/karma.config.js'
+  }, done).start();
+});
+
+// get static files from our app
+gulp.task('staticfiles:dev', function() {
+  return gulp.src('./app/**/*.html')
+    .pipe(gulp.dest('build/'));
+});
+
+// build our bundle when gulp is called by itself on command line
+gulp.task('build:dev', ['staticfiles:dev', 'webpack:dev']);
+gulp.task('default', ['build:dev']);
